3318613ef3e3a3adb50fa4f986c1d2f0b7ecdd76
anderjma <andersonmoal06@gmail.com>
2026-06-13 18:30:16 -0600

    refactor: remove unused catalogService

--- frontend/frontend/src/services/catalogService.ts
+++
@@ -1,9 +0,0 @@
-// Este archivo maneja las peticiones HTTP relacionadas con la consulta pública del catálogo.
-import axiosClient from "../api/axiosClient";
-import type { Product } from "../types/product";
-
-// Esta función recupera la lista de productos disponibles para ser mostrados a usuarios no autenticados.
-export const getPublicProducts = async () => {
-    const response = await axiosClient.get<Product[]>("/Catalog");
-    return response.data;
-};
